a5622083cf0845e4ee7b933bd964b9fc5a4ece42
ElasticSea <info@elasticsea.com>
2020-10-21 23:22:01 +0200

    Setting up AR with separate scene and overlaying depth and camera feed.

---
+++ Assets/InitXr.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.XR.ARFoundation;
+using UnityEngine.XR.Management;
+
+public class InitXr : MonoBehaviour
+{
+    private void Awake()
+    {
+        XRGeneralSettings.Instance.Manager.InitializeLoaderSync();
+        
+        if (XRGeneralSettings.Instance.Manager.activeLoader == null)
+        {
+            Debug.LogError("Initializing XR Failed. Check Editor or Player log for details.");
+        }
+        else
+        {
+            XRGeneralSettings.Instance.Manager.StartSubsystems();
+            LoaderUtility.Initialize();
+            SceneManager.LoadScene("Lidar Feed", LoadSceneMode.Single);
+        }
+    }
+}
---
+++ Assets/SyncTextureRect.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class SyncTextureRect : MonoBehaviour, IDragHandler, IPointerDownHandler
+{
+    [SerializeField] private RectTransform container;
+    [SerializeField] private RawImage texture;
+    
+    private RectTransform textureRt;
+
+    private void Awake()
+    {
+        textureRt = texture.GetComponent<RectTransform>();
+    }
+
+    public void OnDrag(PointerEventData eventData) => SyncRect(eventData.position.y);
+
+    public void OnPointerDown(PointerEventData eventData) => SyncRect(eventData.position.y);
+
+    private void SyncRect(float y)
+    {
+        var heightDelta = y / container.rect.height;
+        texture.uvRect = new Rect(texture.uvRect.x, heightDelta, texture.uvRect.width, 1 - heightDelta);
+
+        textureRt.offsetMax = Vector2.zero;
+        textureRt.offsetMin = new Vector2(textureRt.offsetMin.x, y);
+    }
+}
